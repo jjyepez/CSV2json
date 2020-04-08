f2ed849337c79d8ea96aefc2a1f82e958c15d384
jjyepez <jjyepez@gmail.com>
2020-04-08 00:45:20 -0500

    Grouping by Country and summing values

--- public/js/script.js
+++ public/js/script.js
@@ -19,11 +19,48 @@ async function init() {
 
         let jsonData = json.data;
 
-        json2table({ json: jsonData, selector: '#data' })
+        let keys = jsonData[0] || []; // se asume que el elemento 0 tiene los nombres de campos
+        let allowedKeys = ['Country_Region', 'Country/Region', 'Country', 'Confirmed', 'Deaths', 'Recovered'];
+        let ids = keys.map((key, i) => allowedKeys.includes(key) ? i : undefined).filter(x => x);
+        jsonData = arrayRemoveElements(jsonData, ids);
+
+        jsonData = arrayGroupBy(jsonData, 0);
+
+        json2table({
+            json: jsonData,
+            selector: '#data'
+        })
     })
 }
 
+
+function arrayGroupBy(arrayIn, id) {
+    let objOut = {};
+    arrayIn.forEach(row => {
+        if (!objOut[row[id]]) {
+            objOut[row[id]] = [0, 0, 0]
+        }
+        let rowOut = row.map(r => r); rowOut.shift();
+        objOut[row[id]][0] += parseFloat(0 + rowOut[0]);
+        objOut[row[id]][1] += parseFloat(0 + rowOut[1]);
+        objOut[row[id]][2] += parseFloat(0 + rowOut[2]);
+    })
+    let arrOut = Object.keys(objOut).map(key => {
+        return [key, ...objOut[key]];
+    })
+    return arrOut;
+}
+
+function arrayRemoveElements(array, ids) {
+    if (!ids) return array;
+    let arrayOut = array.map(record => {
+        return record.filter((_, i) => ids.includes(i));
+    })
+    return arrayOut;
+}
+
 function json2table({ json, selector }) {
+
     document.querySelector(selector).innerHTML = '';
     let html = '';
     html = JSON.stringify(json);
