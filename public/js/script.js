c69fb5e1c73d83c1a62a972946986ce7c6901643
jjyepez <jjyepez@gmail.com>
2020-04-08 02:38:30 -0500

    DataTable was added

--- public/js/script.js
+++ public/js/script.js
@@ -20,30 +20,84 @@ async function init() {
         let jsonData = json.data;
 
         let keys = jsonData[0] || []; // se asume que el elemento 0 tiene los nombres de campos
+        jsonData.shift();
+
         let allowedKeys = ['Country_Region', 'Country/Region', 'Country', 'Confirmed', 'Deaths', 'Recovered'];
         let ids = keys.map((key, i) => allowedKeys.includes(key) ? i : undefined).filter(x => x);
+
         jsonData = arrayRemoveElements(jsonData, ids);
 
-        jsonData = arrayGroupBy(jsonData, 0);
+        let calcActives = function (item) { return parseFloat(item[1]) - (parseFloat(item[2]) + parseFloat(item[3])) };
+        let calcDiffDeaths = function (prev, current) { return parseFloat(current[2]) - parseFloat(prev[2]) };
+        let calcDiffConfirm = function (prev, current) { return parseFloat(current[1]) - parseFloat(prev[1]) };
+        jsonData = arrayGroupBy(jsonData, 0, [1, 2, 3, calcActives, calcDiffDeaths]);
+
+        jsonData.sort((a, b) => {
+            return b[1] - a[1];
+        })
+
+        window.data = jsonData
 
         json2table({
             json: jsonData,
-            selector: '#data'
+            selector: '#data',
+            callback: (selector) => initDataTable(selector)
         })
     })
 }
 
+function initDataTable(selector) {
+    selector = `.display`;
+    // try {
+    //     $(selector).DataTable().destroy();
+    // } catch { }
+    $(selector).DataTable({
+        order: [[1, "desc"]]
+    });
+}
 
-function arrayGroupBy(arrayIn, id) {
+function getArgs(func) {
+    // --- Source: https://davidwalsh.name/javascript-arguments
+    // First match everything inside the function argument parens.
+    var args = func.toString().match(/function\s.*?\(([^)]*)\)/)[1];
+
+    // Split the arguments string into an array comma delimited.
+    return args.split(',').map(function (arg) {
+        // Ensure no inline comments are parsed and trim the whitespace.
+        return arg.replace(/\/\*.*\*\//, '').trim();
+    }).filter(function (arg) {
+        // Ensure no undefined values are added.
+        return arg;
+    });
+}
+
+function arrayGroupBy(arrayIn, id, aggIds = []) {
     let objOut = {};
-    arrayIn.forEach(row => {
+    let prevRow = [];
+    arrayIn.forEach((row, j) => {
         if (!objOut[row[id]]) {
-            objOut[row[id]] = [0, 0, 0]
+            objOut[row[id]] = new Array(aggIds.length).fill(0)
         }
-        let rowOut = row.map(r => r); rowOut.shift();
-        objOut[row[id]][0] += parseFloat(0 + rowOut[0]);
-        objOut[row[id]][1] += parseFloat(0 + rowOut[1]);
-        objOut[row[id]][2] += parseFloat(0 + rowOut[2]);
+        let rowOut = row.map(r => r);
+
+        let i = 0;
+        aggIds.forEach(idAgg => {
+            if (idAgg !== id) {
+                if (typeof idAgg === 'function') {
+                    if (getArgs(idAgg).length === 1) {
+                        objOut[row[id]][i] = idAgg(row);
+                    } else if (getArgs(idAgg).length === 2) {
+                        objOut[row[id]][i] = idAgg(prevRow, row);
+                    } else {
+                        alert('error');
+                    }
+                } else {
+                    objOut[row[id]][i] += parseFloat(0 + rowOut[idAgg]);
+                }
+                i++;
+            }
+        })
+        prevRow = row;
     })
     let arrOut = Object.keys(objOut).map(key => {
         return [key, ...objOut[key]];
@@ -59,13 +113,26 @@ function arrayRemoveElements(array, ids) {
     return arrayOut;
 }
 
-function json2table({ json, selector }) {
-
+function json2table({ json, selector, callback }) {
     document.querySelector(selector).innerHTML = '';
     let html = '';
     html = JSON.stringify(json);
 
     let table = document.createElement('table');
+    let tbody = document.createElement('tbody');
+    let thead = document.createElement('thead');
+    thead.innerHTML = `
+        <tr>
+        <th>Country</th>
+        <th>Confirmed</th>
+        <th>Deaths</th>
+        <th>Recovered</th>
+        <th>Active</th>
+        <th>DeathDiff</th>
+        </tr>
+    `;
+    table.setAttribute('id', `${selector}__DataTable`);
+    table.classList.add('display');
     json.forEach(row => {
         //console.log(row);
         let tr = document.createElement('tr');
@@ -74,7 +141,10 @@ function json2table({ json, selector }) {
             td.innerText = value;
             tr.append(td);
         });
-        table.append(tr);
+        tbody.append(tr);
     });
+    table.append(thead)
+    table.append(tbody)
     document.querySelector(selector).append(table);
+    callback && callback(selector, json);
 }
