0630afcb3cf12a2226eca30f166e41ffa55ef37f
jjyepez <jjyepez@gmail.com>
2020-04-07 22:55:56 -0500

    New data arrived

--- public/js/script.js
+++ public/js/script.js
@@ -17,6 +17,27 @@ async function init() {
         const urlEndpoint = `/api/data/${selectedDate}`;
         const json = await fetch(urlEndpoint).then(rslt => rslt.json());
 
-        document.getElementById('data').innerText = JSON.stringify(json.data);
+        let jsonData = json.data;
+
+        json2table({ json: jsonData, selector: '#data' })
     })
+}
+
+function json2table({ json, selector }) {
+    document.querySelector(selector).innerHTML = '';
+    let html = '';
+    html = JSON.stringify(json);
+
+    let table = document.createElement('table');
+    json.forEach(row => {
+        //console.log(row);
+        let tr = document.createElement('tr');
+        row.forEach(value => {
+            let td = document.createElement('td');
+            td.innerText = value;
+            tr.append(td);
+        });
+        table.append(tr);
+    });
+    document.querySelector(selector).append(table);
 }
