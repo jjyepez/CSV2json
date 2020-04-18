68ef9418d32f2f7543d00e76d7848843d9dc1525
jjyepez <jjyepez@gmail.com>
2020-04-18 03:00:50 -0500

    Resuelto el bug en cálculo de activos

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const http = require("http");
+const express = require('express');
+const http = require('http');
 const path = require('path');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const { getDataByDate } = require('./utils/Data');
 
@@ -10,54 +10,54 @@ const app = express();
 
 console.clear();
 
-app
-  .use(express.json())
-  .use(require("morgan")('dev'))
-  .use('/', express.static(path.join(__dirname, '../public')));
+app.use(require('morgan')('dev'))
+    .use(express.json())
+    .use(require('cors')())
+    .use('/', express.static(path.join(__dirname, '../public')));
 
-app
-  .get('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
-  })
+})
 
-  .get('/api/dates', (req, res, next) => {
-    let data = [];
-    let today = new Date();
-    let initDate = new Date(2020, 0, 22);
-    let diff = ~~((today - initDate) / 1000 / 3600 / 24);
+    .get('/api/dates', (req, res, next) => {
+        let data = [];
+        let today = new Date();
+        let initDate = new Date(2020, 0, 22);
+        let diff = ~~((today - initDate) / 1000 / 3600 / 24);
 
-    let date = new Date(today);
-    for (let i = 0; i <= diff; i++) {
+        let date = new Date(today);
+        for (let i = 0; i <= diff; i++) {
+            let dateArr = date.toLocaleDateString('es-CO').split('-');
+            let dateString = `${('0' + dateArr[2]).substr(-2)}-${(
+                '0' + dateArr[1]
+            ).substr(-2)}-${dateArr[0]}`;
+            data.push(dateString);
+            date.setDate(date.getDate() - 1);
+        }
 
-      let dateArr = date.toLocaleDateString('es-CO').split('-');
-      let dateString = `${('0' + dateArr[2]).substr(-2)}-${('0' + dateArr[1]).substr(-2)}-${dateArr[0]}`;
-      data.push(dateString);
-      date.setDate(date.getDate() - 1);
-    }
+        res.json({ today, initDate, diff, data });
+    })
 
-    res.json({ today, initDate, diff, data })
-  })
+    .get('/api/data/:date', async (req, res, next) => {
+        let date = req.params['date'].split('-'); //DD-MM-AAAA
+        let dateISO = new Date(date[2], date[1] - 1, date[0]);
+        let json = {};
+        json = await getDataByDate({ date: dateISO });
+        res.json({ ...json, dateISO });
+    })
 
-  .get("/api/data", async (req, res, next) => {
-    const today = new Date();
-    let json = {};
-    json = await getDataByDate({ date: today, fallback: "yesterday+" });
-    res.json(json);
-  })
+    .get('/api/data', async (req, res, next) => {
+        const today = new Date();
+        let json = {};
+        json = await getDataByDate({ date: today, fallback: 'yesterday+' });
+        res.json(json);
+    })
 
-  .get("/api/data/:date", async (req, res, next) => {
-    let date = req.params['date'].split('-'); //DD-MM-AAAA
-    let dateISO = new Date(date[2], date[1] - 1, date[0]);
-    let json = {};
-    json = await getDataByDate({ date: dateISO });
-    res.json({ ...json, dateISO });
-  })
-
-  .get("*", (req, res, next) => {
-    res.send("404");
-  });
+    .get('*', (req, res, next) => {
+        res.send('404');
+    });
 
 let port = process.env.EXPRESS_PORT || 8080;
 http.createServer(app).listen(port, () => {
-  console.log(`http://localhost:${port}`)
+    console.log(`http://localhost:${port}`);
 }); //the server object listens on port 8080
--- public/js/script.js
+++ public/js/script.js
@@ -1,58 +1,107 @@
 (async () => await init())();
 
 async function init() {
-    let json = await fetch('/api/dates').then(rslt => rslt.json());
+    let json = await fetch('/api/dates').then((rslt) => rslt.json());
     let dates = json.data || [];
     let selectDates = document.getElementById('selectDates');
-    dates.forEach(date => {
+    dates.forEach((date) => {
         let option = document.createElement('option');
         option.value = date;
         option.text = date;
         selectDates.appendChild(option);
-    })
+    });
     selectDates.addEventListener('change', async function (ev) {
         let lista = this;
         let selectedDate = lista.options[lista.selectedIndex].value;
 
         const urlEndpoint = `/api/data/${selectedDate}`;
-        const json = await fetch(urlEndpoint).then(rslt => rslt.json());
+        const json = await fetch(urlEndpoint).then((rslt) => rslt.json());
+
+        console.log({ raw: json });
 
         let jsonData = json.data;
 
         let keys = jsonData[0] || []; // se asume que el elemento 0 tiene los nombres de campos
         jsonData.shift();
 
-        let allowedKeys = ['Country_Region', 'Country/Region', 'Country', 'Confirmed', 'Deaths', 'Recovered'];
-        let ids = keys.map((key, i) => allowedKeys.includes(key) ? i : undefined).filter(x => x);
+        let allowedKeys = [
+            'Country_Region',
+            'Country/Region',
+            'Country',
+            'Confirmed',
+            'Deaths',
+            'Recovered',
+        ];
+        let ids = keys
+            .map((key, i) => (allowedKeys.includes(key) ? i : undefined))
+            .filter((x) => x);
 
         jsonData = arrayRemoveElements(jsonData, ids);
 
-        let calcActives = function (item) { return parseFloat(item[1]) - (parseFloat(item[2]) + parseFloat(item[3])) };
-        let calcDiffDeaths = function (prev, current) { return parseFloat(current[2]) - parseFloat(prev[2]) };
-        let calcDiffConfirm = function (prev, current) { return parseFloat(current[1]) - parseFloat(prev[1]) };
-        jsonData = arrayGroupBy(jsonData, 0, [1, 2, 3, calcActives, calcDiffDeaths]);
+        console.log({ rawJsD: jsonData });
 
+        let calcActives = function (current) {
+            if (
+                typeof total == 'undefined' ||
+                typeof total[selectedDate] == 'undefined' ||
+                typeof total[selectedDate][current[0]] == 'undefined'
+            ) {
+                total = {};
+                total[selectedDate] = {};
+                total[selectedDate][current[0]] = 0;
+            }
+            total[selectedDate][current[0]] +=
+                parseFloat(current[1]) -
+                parseFloat(current[2]) -
+                parseFloat(current[3]);
+
+            return total[selectedDate][current[0]];
+        };
+        let calcDiffDeaths = 0; //function (prev, current) { return parseFloat(current[2]) - parseFloat(prev[2]) };
+        let calcDiffConfirm = 0; //function (prev, current) { return parseFloat(current[1]) - parseFloat(prev[1]) };
+        let calcDiffRecovered = 0; //function (prev, current) { return parseFloat(current[3]) - parseFloat(prev[3]) };
+
+        //jsonData = arrayFilterBy(jsonData, 0, 'US');
+
+        // ordenar por país
         jsonData.sort((a, b) => {
-            return b[1] - a[1];
-        })
+            return b[0] < a[0] ? -1 : b[0] > a[0] ? 1 : 0;
+        });
 
-        window.data = jsonData
+        console.log({ rawJsDS: jsonData });
+
+        jsonData = arrayGroupBy(jsonData, 0, [
+            1,
+            2,
+            3,
+            calcActives,
+            calcDiffConfirm,
+            calcDiffDeaths,
+            calcDiffRecovered,
+        ]);
+
+        window.data = jsonData;
 
         json2table({
             json: jsonData,
             selector: '#data',
-            callback: (selector) => initDataTable(selector)
-        })
-    })
+            callback: (selector) => initDataTable(selector),
+        });
+    });
 }
 
 function initDataTable(selector) {
     selector = `.display`;
-    // try {
-    //     $(selector).DataTable().destroy();
-    // } catch { }
+    try {
+        $(selector).DataTable().destroy();
+    } catch {}
     $(selector).DataTable({
-        order: [[1, "desc"]]
+        order: [[1, 'desc']],
+        scrollY: 500,
+        pageLength: 50,
+        formatNumber: function (toFormat) {
+            return toFormat.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "'");
+        },
     });
 }
 
@@ -62,13 +111,20 @@ function getArgs(func) {
     var args = func.toString().match(/function\s.*?\(([^)]*)\)/)[1];
 
     // Split the arguments string into an array comma delimited.
-    return args.split(',').map(function (arg) {
-        // Ensure no inline comments are parsed and trim the whitespace.
-        return arg.replace(/\/\*.*\*\//, '').trim();
-    }).filter(function (arg) {
-        // Ensure no undefined values are added.
-        return arg;
-    });
+    return args
+        .split(',')
+        .map(function (arg) {
+            // Ensure no inline comments are parsed and trim the whitespace.
+            return arg.replace(/\/\*.*\*\//, '').trim();
+        })
+        .filter(function (arg) {
+            // Ensure no undefined values are added.
+            return arg;
+        });
+}
+
+function arrayFilterBy(arrayIn, id, filterStr) {
+    return arrayIn.filter((item) => item[id] === filterStr);
 }
 
 function arrayGroupBy(arrayIn, id, aggIds = []) {
@@ -76,12 +132,12 @@ function arrayGroupBy(arrayIn, id, aggIds = []) {
     let prevRow = [];
     arrayIn.forEach((row, j) => {
         if (!objOut[row[id]]) {
-            objOut[row[id]] = new Array(aggIds.length).fill(0)
+            objOut[row[id]] = new Array(aggIds.length).fill(0);
         }
-        let rowOut = row.map(r => r);
+        let rowOut = row.map((r) => r);
 
         let i = 0;
-        aggIds.forEach(idAgg => {
+        aggIds.forEach((idAgg) => {
             if (idAgg !== id) {
                 if (typeof idAgg === 'function') {
                     if (getArgs(idAgg).length === 1) {
@@ -96,20 +152,20 @@ function arrayGroupBy(arrayIn, id, aggIds = []) {
                 }
                 i++;
             }
-        })
+        });
         prevRow = row;
-    })
-    let arrOut = Object.keys(objOut).map(key => {
+    });
+    let arrOut = Object.keys(objOut).map((key) => {
         return [key, ...objOut[key]];
-    })
+    });
     return arrOut;
 }
 
 function arrayRemoveElements(array, ids) {
     if (!ids) return array;
-    let arrayOut = array.map(record => {
+    let arrayOut = array.map((record) => {
         return record.filter((_, i) => ids.includes(i));
-    })
+    });
     return arrayOut;
 }
 
@@ -128,23 +184,25 @@ function json2table({ json, selector, callback }) {
         <th>Deaths</th>
         <th>Recovered</th>
         <th>Active</th>
-        <th>DeathDiff</th>
+        <th>Confirmed Diff</th>
+        <th>Deaths Diff</th>
+        <th>Recovered Diff</th>
         </tr>
     `;
     table.setAttribute('id', `${selector}__DataTable`);
     table.classList.add('display');
-    json.forEach(row => {
-        //console.log(row);
+    table.classList.add('stripe');
+    json.forEach((row) => {
         let tr = document.createElement('tr');
-        row.forEach(value => {
+        row.forEach((value) => {
             let td = document.createElement('td');
             td.innerText = value;
             tr.append(td);
         });
         tbody.append(tr);
     });
-    table.append(thead)
-    table.append(tbody)
+    table.append(thead);
+    table.append(tbody);
     document.querySelector(selector).append(table);
     callback && callback(selector, json);
 }
